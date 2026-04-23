a9f5ac5600b9f61569d705b75be5a1d1341438e3
Sahan-A-2003 <sahan.asantha2003@gmail.com>
2026-04-23 09:05:10 +0530

    view task from the json file

--- main.py
+++ main.py
@@ -16,11 +16,26 @@ def save_tasks(tasks):
   except:
     print("faile to save")
 
-def view_tasks():
-  pass
+def view_tasks(tasks):
+  print()
+  tasks_list = tasks["tasks"]
+  if len(tasks_list) == 0:
+    print("No tasks to display.")
+  else:
+    print("You To_Do List: ")
+    for idx, task in enumerate(tasks_list):
+      status = "[Complete]" if task["complete"] else "[Pending]"
+      print(f"{idx + 1}. {task['description']} | {status}")
 
-def create_tasks():
-  pass
+def create_tasks(tasks):
+  description = input("Enter the tasks description: ").strip()
+
+  if description:
+    tasks["tasks"].append({"description":description, "complete": False})
+    save_tasks(tasks)
+    print("Tasks added.")
+  else:
+    print("desription can not be empty..")
 
 def mark_tasks_complete():
   pass
@@ -38,9 +53,9 @@ def main():
     choice = input("Enter your choice: ").strip()
 
     if choice == "1":
-      view_tasks()
+      view_tasks(tasks)
     elif choice == "2":
-      create_tasks()
+      create_tasks(tasks)
     elif choice == "3":
       mark_tasks_complete()
     elif choice == "4":
