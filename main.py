37fe15080f25198d17b20da397398e48605bea4b
Sahan-A-2003 <sahan.asantha2003@gmail.com>
2026-04-23 08:30:54 +0530

    creting the to do list rodemap

---
+++ main.py
@@ -0,0 +1,40 @@
+def lode_tasks():
+  pass
+
+def save_tasks():
+  pass
+
+def view_tasks():
+  pass
+
+def create_tasks():
+  pass
+
+def mark_tasks_complete():
+  pass
+
+def main():
+  tasks = lode_tasks()
+
+  while True:
+    print("\nTo Do List Manager")
+    print("1. View Tasks")
+    print("2. Add Tasks")
+    print("3. Complete Tasks")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ").strip()
+
+    if choice == "1":
+      view_tasks()
+    elif choice == "2":
+      create_tasks()
+    elif choice == "3":
+      mark_tasks_complete()
+    elif choice == "4":
+      print("Goodbye!!")
+      break
+    else:
+      print("Invalide choice. please try againe.")
+
+main()
